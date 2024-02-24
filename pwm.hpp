0aa52d3d2291f6c2499c08db5ab14ce27381c505
sabanekko3 <tetayaae@gmail.com>
2024-02-24 13:52:16 +0900

    add some getter/setter in PWMLLSoft class

--- pwm.hpp
+++ pwm.hpp
@@ -67,21 +67,11 @@ private:
 	uint16_t count = 0;
 	uint16_t period = 0;
 	uint16_t duty = 0xFFFF;
+	bool output_state = false;
 
 public:
 	PWMLLSoft(GPIO_TypeDef *_port,uint16_t _pin,float _min = 0,float _max = 1)
 		: port(_port),pin(_pin),min(_min),max(_max),diff_inv(1/(max - min)){
-	}
-	void out(float val)override{
-		if(val < min || max < val)val  = 0;
-		duty = (val - min)*diff_inv*period;
-	}
-	void set_duty(uint16_t _duty){
-		duty = _duty;
-	}
-	void set_period(uint16_t _period){
-		count = 0;
-		period = _period;
 	}
 
 	void set_input_mode(bool mode){
@@ -89,12 +79,24 @@ public:
 		else LL_GPIO_SetPinMode(port, pin, LL_GPIO_MODE_OUTPUT);
 	}
 
-	bool get_pin_state(void){
-		return LL_GPIO_IsInputPinSet(port,pin);
+	void out(float val)override{
+		if(val < min || max < val)val  = 0;
+		duty = (val - min)*diff_inv*period;
 	}
+
+	void set_duty(const uint16_t _duty){ duty = _duty; }
+	uint16_t get_duty(void){ return duty; }
+
+	void set_period(uint16_t _period){ count = 0; period = _period; }
+	uint16_t get_period(void){ return period; }
+
+	bool get_input_state(void){ return LL_GPIO_IsInputPinSet(port,pin); }
+	void set_output_state(bool state){ output_state = state; }
+
+	//timer interrupt function
 	void update(void){
 		count = count >= period ? 0 : count+1;
-		if(count > duty) LL_GPIO_ResetOutputPin(port,pin);
+		if(count > duty && output_state) LL_GPIO_ResetOutputPin(port,pin);
 		else LL_GPIO_SetOutputPin(port,pin);
 	}
 };
